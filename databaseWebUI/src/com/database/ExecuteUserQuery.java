2b10eddb85fb4659b6d1edd2aff595818eee8d38
Akshay P <38344352+notakshayp@users.noreply.github.com>
2020-11-20 14:22:43 +0530

    Update ExecuteUserQuery.java

--- databaseWebUI/src/com/database/ExecuteUserQuery.java
+++ databaseWebUI/src/com/database/ExecuteUserQuery.java
@@ -2,7 +2,7 @@ package com.database;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
+import java.sql.SQLException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
