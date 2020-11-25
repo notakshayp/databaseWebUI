261c83d8fc0a02aef26724dec9a10e0f6f493113
Akshay P <38344352+notakshayp@users.noreply.github.com>
2020-11-25 18:00:58 +0530

    Update RunQuery.java

--- databaseWebUI/src/com/database/ops/RunQuery.java
+++ databaseWebUI/src/com/database/ops/RunQuery.java
@@ -1,10 +1,11 @@
 package com.database.ops;
-import java.sql.SQLException;
+
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class RunQuery {
 	public String runUserQuery(String dbvendor, String host, String port, String database, String username,
-			String password, String queryEditor)throws SQLException  {
+			String password, String queryEditor) throws SQLException {
 		CreateConnectionObject conObj = new CreateConnectionObject();
 
 		Connection con = null;
@@ -12,6 +13,9 @@ public class RunQuery {
 			con = conObj.genaratePostgresqlConnection(host, port, database, username, password);
 		} else if (dbvendor.equals("MySQL")) {
 			con = conObj.genarateMysqlConnection(host, port, database, username, password);
+		}
+		else if (dbvendor.equals("Oracle")) {
+			con = conObj.genarateOracleConnection(host, port, database, username, password);
 		}
 
 		if (con == null) {
